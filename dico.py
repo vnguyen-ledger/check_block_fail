ca3f23450a124c7d97971404a1c2a51b8ff4909d
vincent nguyen <vincent.nguyen@ledger.fr>
2022-06-09 11:56:48 +0200

    function range_block

--- dico.py
+++ dico.py
@@ -22,6 +22,7 @@ def node_fail_count(d):
     c[keys] = 0
   return c
 
+
 #We can stack all the value and information in last block if we need later
 last_block = requests.get(url='http://localhost:33000/blocks/latest', headers={"Content-type": "application/json"})
 #print(dir(last_block)) allow us to show all the function associated to the object 'last_block'
@@ -29,7 +30,13 @@ last_block = requests.get(url='http://localhost:33000/blocks/latest', headers={"
 last_block_height = last_block.json()["block"]["header"]["height"]
 print(last_block_height)
 
+def range_block(range):
+  low_height = int(last_block_height) - range
+  return low_height
+
+
 def main():
+  print(range_block(10))
   d = fingerprint("ledger", "kraken","binance","fesPFJEPF")
   print(d)
   print(node_fail_count(d))
