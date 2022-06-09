845fc030ef75ddbf9d2c68044edb9d1580f7ed0d
vincent nguyen <vincent.nguyen@ledger.fr>
2022-06-09 11:46:08 +0200

    add current height variable

--- dico.py
+++ dico.py
@@ -4,8 +4,7 @@
 from ast import arg
 import configparser
 from multiprocessing.sharedctypes import Value
-from optparse import Values
-from this import d
+import requests
 config = configparser.ConfigParser()
 config.read('config.cfg')
 
@@ -20,13 +19,19 @@ def fingerprint(*args):
 def node_fail_count(d):
   c = {}
   for keys in d:
-    
-    print(keys, d[keys])
+    c[keys] = 0
   return c
 
+#We can stack all the value and information in last block if we need later
+last_block = requests.get(url='http://localhost:33000/blocks/latest', headers={"Content-type": "application/json"})
+#print(dir(last_block)) allow us to show all the function associated to the object 'last_block'
+#We used the function .json() to have only the code json and not the object 'last block'
+last_block_height = last_block.json()["block"]["header"]["height"]
+print(last_block_height)
 
 def main():
   d = fingerprint("ledger", "kraken","binance","fesPFJEPF")
+  print(d)
   print(node_fail_count(d))
 
 if __name__ == "__main__":
