97932cd549db8bad19764221e5bb753e4f676805
vincent nguyen <vincent.nguyen@ledger.fr>
2022-06-15 12:22:37 +0200

    implementation of 2 version + pretty json display

--- dico.py
+++ dico.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import configparser
+from heapq import merge
 import json
 from multiprocessing import Value
 from platform import node
@@ -27,7 +28,7 @@ config.read('config.cfg')
 #     print(list)
 
 def option_arg():
-  nb = input("enter the range of last block: ")
+  nb = input("enter detail for more detail :  ")
   if nb == 'detail':
     return 1
   return 0
@@ -84,49 +85,59 @@ def missed_block(range_block):
   print(fingerprint)
   print(fail_count)
   print(low_heigth(range_block),last_block_height())
-  for block in range(low_heigth(range_block), last_block_height()):
-    i = 0
-    block_signature = requests.get(url='http://localhost:33000/blocks/%s' %block, headers={"Content-type": "application/json"})
-    # print(dir(block_signature))
-    # print(block_signature.json())
+  detail = option_arg()
+  if detail == 1:
     for keys in fail_count:
-      for keys, values in fingerprint.items():
-      # print(json.dumps(json.loads(block_signature.text), indent=4, sort_keys=True))
-        for signature in block_signature.json()["block"]["last_commit"]["signatures"]:
-        # print(signature["validator_address"])
-          if signature["validator_address"] == values:
-            i = 1
-        if i != 1:
-          fail_count[keys] = fail_count[keys] +1
-          print(int(block_signature.json()["block"]["header"]["height"])-1)
+      list_block_height = []
+      height = {'block_missed' : 0, 'height' : list_block_height }
+      fail_count[keys] = height
+    print(fail_count)
+    for block in range(low_heigth(range_block), last_block_height()):
+      i = 0
+      block_signature = requests.get(url='http://localhost:33000/blocks/%s' %block, headers={"Content-type": "application/json"})
+      # print(dir(block_signature))
+      # print(block_signature.json())
+      for keys in fail_count:
+        for key, values in fingerprint.items():
+        # print(json.dumps(json.loads(block_signature.text), indent=4, sort_keys=True))
+          for signature in block_signature.json()["block"]["last_commit"]["signatures"]:
+          # print(signature["validator_address"])
+            if signature["validator_address"] == values:
+              i = 1
+          if i != 1:
+            fail_count[keys]["block_missed"] = fail_count[keys]["block_missed"] +1
+            fail_count[keys]['height'].append(int(block_signature.json()["block"]["header"]["height"])-1)
+            print(int(block_signature.json()["block"]["header"]["height"])-1)
+  else:
+    for block in range(low_heigth(range_block), last_block_height()):
+      i = 0
+      block_signature = requests.get(url='http://localhost:33000/blocks/%s' %block, headers={"Content-type": "application/json"})
+      # print(dir(block_signature))
+      # print(block_signature.json())
+      for keys in fail_count:
+        for keys, values in fingerprint.items():
+        # print(json.dumps(json.loads(block_signature.text), indent=4, sort_keys=True))
+          for signature in block_signature.json()["block"]["last_commit"]["signatures"]:
+          # print(signature["validator_address"])
+            if signature["validator_address"] == values:
+              i = 1
+          if i != 1:
+            fail_count[keys] = fail_count[keys] +1
+            print(int(block_signature.json()["block"]["header"]["height"])-1)
   print("\nnumber of missed block for each node : ")
   print(fail_count)
-  return fail_count
-
-# def option(fail_count):
-#   dict = {}
-#   for keys, values in fail_count.items():
-#     validator = {}
-#     validator[keys] = fail_count[keys]
-#     node = {}
-#     validator[values] = node
-#     for keys, values in node.items():
-#       node[keys] = 'block_missed'
-#       node[values] = 1
-
-#     dict = {**dict, **validator}
-#   print(dict)
-#   return dict
+  dict = { "validator": fail_count}
+  print(dict)
+  return dict
 
 def main():
   arg_range = range_arg()
   range = {'range' : arg_range}
   print(arg_range)
   missed = missed_block(arg_range)
-  merge = {'validator' : missed}
-  dict_json = {** range, ** merge}
+  merge = {**range,**missed}
   with open('data.json', 'w') as file:
-    json.dump(dict_json, file, indent=4)
+    json.dump(merge, file, indent=4)
 
 if __name__ == "__main__":
     main()
